0635d8380d07e5da7342da83bb91592b8b216f5f
bhanubhargavbhas <bhanuprakash339@gmail.com>
2022-02-26 15:02:05 +0530

    hrllo

---
+++ super_testtemplate.tf
@@ -0,0 +1,98 @@
+provider "aws" {
+  region     = "us-east-2"
+  #alias="hellotest"
+  access_key = "AKIA366UABK23NS63G2Z"
+  secret_key = "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"
+}
+
+resource "aws_security_group" "second_sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+
+resource "aws_security_group" "first_sg" {
+  name        = "allow_tls123"
+  description = "Allow TLS inbound traffic"
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_instance" "Own_FirstInstance"{
+		ami="ami-0b614a5d911900a9b"
+		instance_type="t2.micro"
+    #aws_security_group="allow_tls"
+    vpc_security_group_ids = [aws_security_group.first_sg.id]
+
+		tags = {
+			Name ="First_Instance"
+		}
+	}
+
+  resource "aws_instance" "Own_SecondInstance"{
+  		ami="ami-0b614a5d911900a9b"
+  		instance_type="t2.micro"
+      #aws_security_group="allow_tls"
+      vpc_security_group_ids = [aws_security_group.second_sg.id]
+
+  		tags = {
+  			Name ="Second_Instance"
+  		}
+  	}
