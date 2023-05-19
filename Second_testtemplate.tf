0c94e7727cb28ef181107bd433d0b9a53ba1fee2
bhanubhargavbhas <98385665+bhanubhargavbhas@users.noreply.github.com>
2023-05-19 13:11:17 +0530

    Update Sample test

--- Second_testtemplate.tf
+++ Second_testtemplate.tf
@@ -108,3 +108,8 @@ resource "aws_key_pair" "deployer" {
   key_name   = "id_rsa"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC59ENB8hYlLa6qFgMIMgZ+AZfDRavYAwiIYkolOPI6DSA4Cs10G4WzirfvZzOYWRwEbjXRdLTOpTne6FCGjkQGVHC7DxWev1In/TvrZte1119ZtM58KWOKr+HELYBUFGA3Vb+bg32AjCg/iULTKEkpuNRHlYNSXH0EDbwALErfYF3aT0OY5RdsII/qLt3h5l8sA89YwfFMydBzUYo18pv24RqafNFVEC9ZafdiXODLqDPJjEzE6GNj/9jSiVspY4KoWPQNRu3o4JEijDeyZY3G5mhskEvSen6CnvxmvkcSptndbvYWe3a780MBvxpTYSyuX9+F5PfKa7j/z4YykIpnj7ZSHqGD+NCbxXOPoAHEi308MoOumk5OiTm++Xq+a5OyhU6Hw76ufQ2px8UWjwnGAzK7t9UyVbHrcrDGMTltTgo50cgnbwrEZ5HgjWcW58PgpukXeh61ExddwJozrohR15aZ3IB20oX7mPvtpKI8BYPbpFZcwnoaD0ys2vVkYhU= ubuntu@ip-172-31-12-12"
 }
+
+
+reosurce "aws_instance" "Sampletest"
+{
+}
